b3af3924c5f604078df942380afc25bdd1c46dad
nazirdogan <nazrdogan@gmail.com>
2015-04-22 10:11:37 +0300

    first release

---
+++ index.js
@@ -0,0 +1,39 @@
+var readline = require('readline');
+var request = require("request");
+var rl = readline.createInterface(process.stdin, process.stdout);
+console.log("Please  Enter app  id:")
+rl.setPrompt('nazir> ');
+rl.prompt();
+rl.on('line', function(line) {
+
+  switch(line.trim()) {
+    case 'exit':
+      process.exit(0);
+      break;
+    default:
+      request("https://play.google.com/store/apps/details?id="+line.trim(), function(error, response, body) {
+          var res=JSON.stringify(response.statusCode);
+
+
+          if(res==='200'){
+            console.log("Application exits on Google Play");
+            rl.prompt();
+          }
+          else if(res==='404'){
+            console.log("Application does not exits on Google Play");
+            rl.prompt();
+          }
+          else{
+            console.log("Please  send this response to me:"+res);
+          }
+          rl.prompt();
+        });
+          break;
+      }
+
+  rl.prompt();
+
+}).on('close', function() {
+
+  process.exit(0);
+});
